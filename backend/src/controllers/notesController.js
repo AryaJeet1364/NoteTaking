829ee4d67f260bf66dacb1acd19ea00c45a67254
AryaJeet1364 <aryajeet.jha@gmail.com>
2025-06-16 11:53:44 +0530

    First commit

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,17 @@
+export const getAllNotes = (req,res) =>{
+    res.status(200).send("You just fetched the notes!!")
+}
+
+
+export const createNote = (req, res) => {
+  res.status(201).json({ message: "Note created successfully! " });
+};
+
+
+export const updateNote = (req, res) => {
+  res.status(200).json({ message: "Note updated successfully! " });
+};
+
+export const deleteNode = (req, res) => {
+  res.status(200).json({ message: "Note deleted successfully! " });
+};
---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,52 @@
+import express from "express";
+import {
+  getAllNotes,
+  createNote,
+  updateNote,
+  deleteNode,
+} from "../controllers/notesController.js";
+
+const router = express.Router();
+
+router.get("/", getAllNotes);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNode);
+
+// router.get("/", (req, res) => {
+//   res.status(200).send("You just fetched the notes");
+// });
+
+// router.post("/", (req, res) => {
+//   res.status(201).json({ message: "Note created successfully! " });
+// });
+
+// router.put("/:id", (req, res) => {
+//   res.status(200).json({ message: "Note updated successfully! " });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   res.status(200).json({ message: "Note deleted successfully! " });
+// });
+
+// app.get("/api/notes", (req,res)=>{
+//     // see the notes
+//     res.status(200).send("U got 20 notes")
+// })
+
+// app.post("/api/notes", (req, res) => {
+//   // create the notes
+//   res.status(201).json({message: "note created successfully"});
+// });
+
+// app.put("/api/notes/:id", (req, res) => {
+//   // update the notes
+//   res.status(200).json({ message: "note updatedc successfully" });
+// });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//   // delete the notes
+//   res.status(200).json({ message: "note deleted successfully" });
+// });
+
+export default router;
